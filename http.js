65b01868a07801a21c8fe1654e342b9451ea08c9
Anton <abodyk1516@gmail.com>
2024-03-13 13:15:24 +0200

    add routing and init npm packages

--- http.js
+++ http.js
@@ -1,24 +1,51 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
+
 
 const server = http.createServer((req, res) => {                //запуск сервера
     console.log('Server request');
     console.log(req.url, req.method);
 
-    res.setHeader('Content-Type', 'application/json');   // вспомогательный метод для записи данных ответа (тип данных)
-    //
-    // res.write('<link rel="stylesheet" href="#">');
-    // res.write('<h1>Hello Node JS users</h1>');
-    // res.write('<p>My name Mark</p>');
+    const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`); //для кроссплатформенной работы сервера
+    let basePath = '';
 
-    const data = JSON.stringify([
-        {id:1, name: 'Tom', age: 20},
-        {id:2, name: 'Brad', age: 40},
-        {id:3, name: 'Danil', age: 30},
-    ])
-    res.end(data)
+    res.setHeader('Content-Type', 'text/html');   // вспомогательный метод для записи данных ответа (тип данных)
+
+    switch (req.url){
+        case '/':
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contacts');
+            res.end();
+            break;
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('errors');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
+
 });
+
 
 server.listen(PORT, 'localhost' , (error) => {       //для прослушивания сообщ серва
     error ? console.log(error) : console.log('listen port 3000');
